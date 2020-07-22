f14d6678d1efb3bed38c37d98d6f84fd9349aa44
achaplyak <andriychaplyak@gmail.com>
2020-07-22 19:26:47 +0300

    lesson 5 part, cars

--- lesson6/task6JS.js
+++ lesson6/task6JS.js
@@ -89,12 +89,44 @@ let newUsersNamesDown = users.sort((a, b) => b.name.length - a.name.length);
 // - пройтись по ньому та додати кожному юзеру поле id - яке характеризує унікальний індентифікатор
 // (По якому принципу його створювати - ваше рішення),
 // та зберегти це в новий масив (первинний масив залишиться без змін)
+
+let newArrUsrs = JSON.parse(JSON.stringify(users));
+
+let myUsers = newArrUsrs.map((user, index) => {
+    user.id = index;
+    return user;
+});
+
+console.log(myUsers);
+
 // - відсортувати його за індентифікатором
-//
-//
+
+myUsers.sort((a, b) => a.id - b.id)
+
 // -- наисать функцию калькулятора с 2мя числами и колбеком
+
+function cal(a, b, callback) {
+    return callback(a, b);
+}
+
+let result = cal(17, 21, function (a, b) {
+    return a + b;
+});
+
+console.log(result);
+
 // -- наисать функцию калькулятора с 3мя числами и колбеком
-//
+
+function calScnd(a, b, c, callback) {
+    return callback(a, b, c);
+}
+
+let resultScnd = calScnd(17, 21, 2, function (a, b, c) {
+    return a + b + c;
+});
+
+console.log(resultScnd);
+
 // =============================================
 // =============================================
 // =============КЛАССНАЯ РАБОТА=================
@@ -104,10 +136,158 @@ let newUsersNamesDown = users.sort((a, b) => b.name.length - a.name.length);
 //
 //
 //
-// let cars = [ {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400}, {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250}, {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300}, {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140}, {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200}, {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165}, {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120}, {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120}, {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350}, {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180}, {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180}, {producer:"mercedes",model: "e63",year 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400}, {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230} ];
+let cars = [
+        {
+            producer: "subaru",
+            model: "wrx",
+            year: 2010,
+            color: "blue",
+            type: "sedan",
+            engine: "ej204x",
+            volume: 2,
+            power: 400
+        },
+        {
+            producer: "subaru",
+            model: "legacy",
+            year: 2007,
+            color:"silver",
+            type: "sedan",
+            engine: "ez30",
+            volume: 3,
+            power: 250
+        },
+        {
+            producer: "subaru",
+            model: "tribeca",
+            year: 2011,
+            color: "white",
+            type: "jeep",
+            engine: "ej20",
+            volume: 2,
+            power: 300
+        },
+
+        {
+            producer: "subaru",
+            model: "leone",
+            year: 1998,
+            color: "yellow",
+            type: "sedan",
+            engine: "ez20x",
+            volume: 2,
+            power: 140
+        },
+
+        {
+            producer: "subaru",
+            model: "impreza",
+            year: 2014,
+            color: "red",
+            type: "sedan",
+            engine: "ej204x",
+            volume: 2,
+            power: 200
+        },
+
+        {
+            producer: "subaru",
+            model: "outback",
+            year: 2014,
+            color: "red",
+            type: "hachback",
+            engine: "ej204",
+            volume: 2,
+            power: 165
+        },
+
+        {
+            producer: "bmw",
+            model: "115",
+            year: 2013,
+            color: "red",
+            type: "hachback",
+            engine: "f15",
+            volume: 1.5,
+            power: 120
+        },
+
+        {
+            producer: "bmw",
+            model: "315",
+            year: 2010,
+            color: "white",
+            type: "sedan",
+            engine: "f15",
+            volume: 1.5,
+            power: 120
+        },
+
+        {
+            producer: "bmw",
+            model: "650",
+            year: 2009,
+            color: "black",
+            type: "coupe",
+            engine: "f60",
+            volume: 6,
+            power: 350
+        },
+
+        {
+            producer: "bmw",
+            model: "320",
+            year: 2012,
+            color: "red",
+            type: "sedan",
+            engine: "f20",
+            volume: 2,
+            power: 180
+        },
+
+        {
+            producer: "mercedes",
+            model: "e200",
+            year: 1990,
+            color: "silver",
+            type: "sedan",
+            engine: "eng200",
+            volume: 2,
+            power: 180
+        },
+
+        {
+            producer: "mercedes",
+            model: "e63",
+            year: 2017,
+            color: "yellow",
+            type: "sedan",
+            engine: "amg63",
+            volume:3,
+            power: 400
+        },
+
+        {
+            producer: "mercedes",
+            model: "c250",
+            year: 2017,
+            color: "red",
+            type: "sedan",
+            engine: "eng25",
+            volume: 2.5,
+            power: 230
+        }
+    ];
 // Відфільтрувати масив за наступними крітеріями :
 //     - двигун більше 3х літрів
+
+let volumeThree = cars.filter(item => item.volume >= 3);
+
 // - двигун = 2л
+
+let volumeTwo = cars.filter(item => item.volume === 2);
+console.log(volumeTwo);
+
 // - виробник мерс
 // - двигун більше 3х літрів + виробник мерседес
 // - двигун більше 3х літрів + виробник субару
